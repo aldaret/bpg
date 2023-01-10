49cd867d97369fde9c999de9edaf5e49b524c6ee
Aleksander Zhukov <46674077+aldaret@users.noreply.github.com>
2023-01-10 22:48:10 +0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "jhukov1r_bpg");
-define("DB_PASSWORD", "z*hl9w7I");
-define("DB_DATABASE_NAME", "jhukov1r_bpg");
-
-define("COUNTRY_CODES", file_get_contents(__DIR__ . '/vendor/CountryCodes.json'))
-?>
